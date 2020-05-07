f37867b5366b00435415320a792095e897d62eb1
axdr3 <xadber@gmail.com>
2020-05-07 15:02:39 -0300

    Make functional_tests an app, use LiveServerTestCase

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,10 +1,11 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import unittest
+from django.test import LiveServerTestCase
+# import unittest
 import time
 
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
 	def setUp(self):
 		self.browser = webdriver.Firefox()
 		self.browser.implicitly_wait(3)
@@ -18,7 +19,7 @@ class NewVisitorTest(unittest.TestCase):
 		self.assertIn(row_text, [row.text for row in rows])
 
 	def test_can_start_a_list_and_retrieve_it_later(self):
-		self.browser.get('http://localhost:8000')
+		self.browser.get(self.live_server_url)
 
 		self.assertIn('To-Do', self.browser.title)
 		header_text = self.browser.find_element_by_tag_name('h1').text
@@ -33,7 +34,7 @@ class NewVisitorTest(unittest.TestCase):
 
 		# She types'Buy peacock feathers' into a text-box
 		inputbox.send_keys('Buy peacock feathers')
-		
+
 		# When she hits enter , the page updates, and now the page lists
 		# "1: Buy peacock feathers" as an item in a todo list
 		inputbox.send_keys(Keys.ENTER)
@@ -51,12 +52,11 @@ class NewVisitorTest(unittest.TestCase):
 
 		time.sleep(5)
 
+		# The page updates again, shows both items
 		self.check_for_row_in_list_table('1: Buy peacock feathers')
 		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
-
 		self.fail('Finish!x')
-		# The page updates again, shows both items
 
 		# Edith wonders if the site will remember her list. Then she sees
 		# that the site has generated a unique URL for her -- there is some
@@ -65,7 +65,3 @@ class NewVisitorTest(unittest.TestCase):
 		# She visits that URL -- her todo list still there
 
 		# Satisfied she goes back to sleep
-
-
-if __name__ == '__main__':
-	unittest.main(warnings='ignore')
