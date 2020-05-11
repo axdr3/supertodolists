81e4a1e2acfa67fcf2d6bfb7b04392f8f787f7f4
axdr3 <xadber@gmail.com>
2020-05-11 19:18:27 -0300

    Fixed tearDownClass. FT works

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -22,7 +22,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 	@classmethod
 	def tearDownClass(cls):
 		if cls.server_url == cls.live_server_url:
-			super.tearDownClass()
+			super().tearDownClass()
 
 	def setUp(self):
 		self.browser = webdriver.Firefox()
