de6ecb8d30bab335ccb1f69684025c8d9bd0dc43
axdr3 <xadber@gmail.com>
2020-05-12 15:35:46 -0300

    Fixed time.wait's, enroute to deploy

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,44 +1,44 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from django.test import LiveServerTestCase
+# from django.test import LiveServerTestCase
+from selenium.common.exceptions import WebDriverException
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 # import unittest
 import time
-import sys
+import os
+
+
+MAX_WAIT = 10
 
 
 class NewVisitorTest(StaticLiveServerTestCase):
 
-	@classmethod
-	def setUpClass(cls):
-		for arg in sys.argv:
-			if 'liveserver' in arg:
-				cls.server_url = 'http://' + arg.split('=')[1]
-				return 
-
-		super().setUpClass()
-		cls.server_url = cls.live_server_url
-
-	@classmethod
-	def tearDownClass(cls):
-		if cls.server_url == cls.live_server_url:
-			super().tearDownClass()
-
 	def setUp(self):
+
 		self.browser = webdriver.Firefox()
-		self.browser.implicitly_wait(3)
+		# staging_server = os.environ.get('STAGING_SERVER')
+		# if staging_server:
+		# 	self.live_server_url = 'http://' + staging_server
 
 	def tearDown(self):
 		self.browser.quit()
 
-	def check_for_row_in_list_table(self, row_text):
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertIn(row_text, [row.text for row in rows])
+	def wait_for_row_in_list_table(self, row_text):
+		start_time = time.time()
+		while True:
+			try:
+				table = self.browser.find_element_by_id('id_list_table')
+				rows = table.find_elements_by_tag_name('tr')
+				self.assertIn(row_text, [row.text for row in rows])
+				return
+			except (AssertionError, WebDriverException) as e:
+				if time.time() - start_time > MAX_WAIT:
+					raise e
+				time.sleep(0.5)
 
 	def test_layout_and_styling(self):
 		# Edith goes to the home page
-		self.browser.get(self.server_url)
+		self.browser.get(self.live_server_url)
 		self.browser.set_window_size(1024, 768)
 
 		# She notices the input box is nicely centered
@@ -60,7 +60,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 		)
 
 	def test_can_start_a_list_and_retrieve_it_later(self):
-		self.browser.get(self.server_url)
+		self.browser.get(self.live_server_url)
 
 		self.assertIn('To-Do', self.browser.title)
 		header_text = self.browser.find_element_by_tag_name('h1').text
@@ -79,12 +79,12 @@ class NewVisitorTest(StaticLiveServerTestCase):
 		# When she hits enter , the page updates, and now the page lists
 		# "1: Buy peacock feathers" as an item in a todo list
 		inputbox.send_keys(Keys.ENTER)
-		time.sleep(2)
+		self.wait_for_row_in_list_table('1: Buy peacock feathers')
+
 		edith_list_url = self.browser.current_url
 		self.assertRegex(edith_list_url, '/lists/.+')
 		# There is still a text box inviting her to add another item. She
 		# enters "Use peacock feathers to make a fly"
-		time.sleep(2)
 		inputbox = self.browser.find_element_by_id('id_new_item')
 		self.assertEqual(
 			inputbox.get_attribute('placeholder'),
@@ -94,11 +94,11 @@ class NewVisitorTest(StaticLiveServerTestCase):
 		inputbox.send_keys('Use peacock feathers to make a fly')
 		inputbox.send_keys(Keys.ENTER)
 
-		time.sleep(2)
+		(2)
 
 		# The page updates again, shows both items
-		self.check_for_row_in_list_table('1: Buy peacock feathers')
-		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+		self.wait_for_row_in_list_table('1: Buy peacock feathers')
+		self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
 		# Now a new user, Francis, comes along to the site
 
@@ -108,7 +108,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 		self.browser = webdriver.Firefox()
 
 		# Francis visits the home page. There is no sign of Edith's list
-		self.browser.get(self.server_url)
+		self.browser.get(self.live_server_url)
 		page_text = self.browser.find_element_by_tag_name('body').text
 		self.assertNotIn('Buy peacock feathers', page_text)
 		self.assertNotIn('make a fly', page_text)
@@ -120,9 +120,9 @@ class NewVisitorTest(StaticLiveServerTestCase):
 		inputbox.send_keys(Keys.ENTER)
 
 		# Francis get his own unique URL
-		time.sleep(2)
+		
+		time.sleep(0.1)
 		francis_list_url = self.browser.current_url
-		time.sleep(2)
 		self.assertRegex(francis_list_url, '/lists/.+')
 		self.assertNotEqual(francis_list_url, edith_list_url)
 
