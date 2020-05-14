ab0103ce2cc121d653b2c34b162f41344b51e469
axdr3 <xadber@gmail.com>
2020-05-14 15:24:15 -0300

    Provisioning script underconstruction

---
+++ deploy_tools/provision_script.sh
@@ -0,0 +1,30 @@
+#! /bin/sh
+#requires to be inside deploy_tools folder
+if [ "$1" == ""]; then
+	exit 1
+
+
+# first arg
+domain = $1
+#second arg
+user = $2
+
+cat nginx.template.conf \
+| sed "s/DOMAIN/$domain/g" \
+| sed "s/USER/$user/g" \
+| sudo tee /etc/nginx/sites-available/$domain
+
+sudo ln -s /etc/nginx/sites-available/$domain \
+    /etc/nginx/sites-enabled/$domain
+
+cat  gunicorn-systemd.template.service \
+| sed "s/DOMAIN/$domain/g" \
+| sed "s/USER/$user/g" \
+| sudo tee /etc/systemd/system/gunicorn-$domain.service
+
+sudo systemctl daemon-reload
+sudo systemctl reload nginx
+sudo systemctl enable gunicorn-$domain
+sudo systemctl restart gunicorn-$domain
+
+exit 0
