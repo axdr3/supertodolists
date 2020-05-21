01eb100e4f974647aa91db8f0b8d838115d01648
axdr3 <xadber@gmail.com>
2020-05-21 16:01:03 -0300

    enforce model validation in list view - FT not passing

--- lists/views.py
+++ lists/views.py
@@ -15,10 +15,17 @@ def home_page(request):
 
 def view_list(request, list_id):
 	list_ = List.objects.get(id=list_id)
+	error = None
 	if request.method == 'POST':
-		Item.objects.create(text=request.POST['item_text'], list=list_)
-		return redirect(f'/lists/{list_.id}/')
-	return render(request, 'lists/list.html', {'list': list_})
+		try:
+			item = Item.objects.create(text=request.POST['item_text'], list=list_)
+			item.full_clean()
+			item.save()
+			return redirect(f'/lists/{list_.id}/')
+		except ValidationError:
+			error = "You can't have an empty list item"
+
+	return render(request, 'lists/list.html', {'list': list_, 'error': error})
 
 
 def new_list(request):
@@ -30,7 +37,7 @@ def new_list(request):
 	except ValidationError:
 		list_.delete()
 		error = "You can't have an empty list item"
-		return render(request, 'lists/home.html', {"error": error})
+		return render(request, 'lists/home.html', {'error': error})
 
 	# return redirect('/lists/%d/' % (list_.id,))
 	return redirect(f'/lists/{list_.id}/')
