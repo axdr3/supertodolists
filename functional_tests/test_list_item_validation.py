99f24741e8872c31d54f46ed7e7c34e50eca678a
axdr3 <xadber@gmail.com>
2020-05-22 15:14:51 -0300

    Implement duplicate item validation at model layer

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -44,3 +44,28 @@ class ItemValidationTest(FunctionalTest):
 		self.get_item_input_box().send_keys(Keys.ENTER)
 		self.wait_for_row_in_list_table('1: Buy milk')
 		self.wait_for_row_in_list_table('2: Make tea')
+
+	def test_cannot_add_duplicate_items(self):
+
+		self.browser.get(self.live_server_url)
+		self.get_item_input_box().send_keys('Buy weed')
+		self.get_item_input_box().send_keys(Keys.ENTER)
+		self.wait_for_row_in_list_table('1: Buy weed')
+
+		# Try adding same item
+		self.get_item_input_box().send_keys('Buy weed')
+		self.get_item_input_box().send_keys(Keys.ENTER)
+
+		time.sleep(0.5)
+
+		self.wait_for(lambda: self.assertEqual(
+		self.browser.find_element_by_css_selector('.show-errors').text,
+		"You've already got this in your list")
+		)
+
+		# Add different item
+		self.get_item_input_box().send_keys('Buy dope')
+		self.get_item_input_box().send_keys(Keys.ENTER)
+		# Verify it hasn't accepted same item
+		self.wait_for_row_in_list_table('1: Buy weed')
+		self.wait_for_row_in_list_table('2: Buy dope')
--- lists/models.py
+++ lists/models.py
@@ -12,3 +12,14 @@ class List(models.Model):
 class Item(models.Model):
 	text = models.TextField(default='')
 	list = models.ForeignKey(List, blank=True, null=True, on_delete=models.CASCADE)
+
+	# Just like ModelForms, models have a class Meta, and that’s where we can implement
+	# a constraint which says that an item must be unique for a particular list, or in
+	# other words, that text and list must be unique together
+
+	class Meta:
+		ordering = ('id',)
+		unique_together = ('list', 'text')
+
+	def __str__(self):
+		return self.text
