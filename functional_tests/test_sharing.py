8b89d936f9544ae3526d0c62cf72023769ac051d
Axdre <axdber@gmail.com>
2020-06-23 12:02:24 -0300

    OK: FT, UT; Sharing lists OK

--- functional_tests/test_sharing.py
+++ functional_tests/test_sharing.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from .base import FunctionalTest
 from .list_page import ListPage
 from .my_lists_page import MyListsPage
-
+import time
 
 def quit_if_possible(browser):
     try:
@@ -19,12 +19,7 @@ class SharingTest(FunctionalTest):
         edith_browser = self.browser
         self.addCleanup(lambda: quit_if_possible(edith_browser))
 
-        # Her friend Oniciferous is also hanging out on the lists site
-        oni_browser = webdriver.Firefox()
-        self.addCleanup(lambda: quit_if_possible(oni_browser))
-        self.browser = oni_browser
-        self.create_pre_authenticated_session('oniciferous@example.com')
-
+       
         # Edith goes to the home page and starts a list
         self.browser = edith_browser
         self.browser.get(self.live_server_url)
@@ -40,21 +35,36 @@ class SharingTest(FunctionalTest):
             'your-friend@example.com'
         )
 
+        # time.sleep(10)
+        # Her friend Oniciferous is also hanging out on the lists site
+        oni_browser = webdriver.Firefox()
+        self.browser = oni_browser
+        self.addCleanup(lambda: quit_if_possible(oni_browser))
+        self.create_pre_authenticated_session('oniciferous@example.com')
+        self.browser.get(self.live_server_url)
+
+        oni_browser = self.browser
+        # time.sleep(10)
+
+        self.browser = edith_browser
         # She shares her list.
         # The page updates to say that it's shared with Oniciferous
         list_page.share_list_with('oniciferous@example.com')
+        edith_browser = self.browser
 
         # Oniciferous now goes to the lists page with his browser
         self.browser = oni_browser
-        MyListsPage(self).go_to_my_lists_page()
+        self.browser.find_element_by_link_text('My lists').click()
+        # print(self.live_server_url)
+        # MyListsPage(self).go_to_my_lists_page()
 
         # He sees Edith's list in there!
         self.browser.find_element_by_link_text('Get help').click()
 
         # On the list page, Oniciferous can see says that it's Edith's list
-        self.wait_for(lambda: self.assertEqual(
-            list_page.get_list_owner(),
-            'edith@example.com'
+        self.wait_for(lambda: self.assertIn(
+            'edith@example.com',
+            list_page.get_list_owner()
         ))
 
         # He adds an item to the list
